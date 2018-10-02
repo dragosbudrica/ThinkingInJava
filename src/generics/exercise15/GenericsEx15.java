9eac24173d7772ddcba4bf5205b71ac35d8ced86
dragosbudrica <budricadragos@yahoo.com>
2018-10-02 17:50:46 +0300

    Generics Exercise 15, 16

---
+++ src/generics/exercise15/GenericsEx15.java
@@ -0,0 +1,7 @@
+package generics.exercise15;
+
+public class GenericsEx15 {
+    public static void main(String[] args) {
+        TupleTest2.main(args);
+    }
+}
---
+++ src/generics/exercise16/GenericsEx16.java
@@ -0,0 +1,7 @@
+package generics.exercise16;
+
+public class GenericsEx16 {
+    public static void main(String args[]) {
+        TupleTest2.main(args);
+    }
+}
